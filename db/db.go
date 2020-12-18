577f948f6915909a8db204796841b0b6a9d1ecb5
Omar Cornejo <omarkhd@uber.com>
2020-12-17 20:42:26 -0600

    grafana and basic dashboard added

--- db/db.go
+++ db/db.go
@@ -4,14 +4,32 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
 	_ "modernc.org/sqlite"
+	"omarkhd/litess/metrics"
 )
 
 const (
 	driverName     = "sqlite"
 	dataSourceName = "db.sqlite"
 )
+
+var (
+	dbQueriesSummary = prometheus.NewSummaryVec(prometheus.SummaryOpts{
+		Name:       "db_queries",
+		Objectives: metrics.Quantiles,
+	}, []string{"procedure"})
+	dbErrorsCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "db_errors",
+	}, []string{"procedure"})
+)
+
+func init() {
+	prometheus.MustRegister(dbQueriesSummary)
+	prometheus.MustRegister(dbErrorsCounter)
+}
 
 type Engine interface {
 	Exec(context.Context, string) (sql.Result, error)
@@ -32,8 +50,17 @@ type liteEngine struct {
 
 func (le *liteEngine) Exec(ctx context.Context, q string) (sql.Result, error) {
 	log.Print(q)
+	labels := map[string]string{
+		"procedure": "exec",
+	}
+	summary := dbQueriesSummary.With(labels)
+	start := time.Now().UnixNano()
+	defer func() {
+		summary.Observe(float64(time.Now().UnixNano() - start))
+	}()
 	r, err := le.db.ExecContext(ctx, q)
 	if err != nil {
+		dbErrorsCounter.With(labels).Inc()
 		return nil, err
 	}
 	return r, nil
@@ -41,8 +68,17 @@ func (le *liteEngine) Exec(ctx context.Context, q string) (sql.Result, error) {
 
 func (le *liteEngine) Query(ctx context.Context, q string) (*sql.Rows, error) {
 	log.Print(q)
+	labels := map[string]string{
+		"procedure": "query",
+	}
+	summary := dbQueriesSummary.With(labels)
+	start := time.Now().UnixNano()
+	defer func() {
+		summary.Observe(float64(time.Now().UnixNano() - start))
+	}()
 	r, err := le.db.QueryContext(ctx, q)
 	if err != nil {
+		dbErrorsCounter.With(labels).Inc()
 		return nil, err
 	}
 	return r, nil
--- metrics/metrics.go
+++ metrics/metrics.go
@@ -9,6 +9,8 @@ import (
 
 const metricsPort = ":9100"
 
+var Quantiles = map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001}
+
 func Expose() {
 	http.Handle("/metrics", promhttp.Handler())
 	log.Printf("Serving metrics on %s", metricsPort)
